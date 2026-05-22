cca39cb9e253c9b31b1f1767472684b9807dccf9
Viktor0902D <viktor0902d@gmail.com>
2026-05-22 18:14:02 +0300

    The double API fetching weather app. Next is more complex project

--- APIsPracticing/WeatherAppWithAPI/api.js
+++ APIsPracticing/WeatherAppWithAPI/api.js
@@ -14,5 +14,25 @@ export async function getWeather(lattitude, longitude) {
     }
 }
 
+ export async function getCoordinates(city){
+    try {
+        const response=await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1`);
+        if(!response.ok){
+            throw new Error(`Failed to fetch coordinates. Status: ${response.status}`);
+        }
+        const coordinatesData=await response.json();
+         if(coordinatesData.results && coordinatesData.results.length > 0){
+            const { latitude, longitude,name } = coordinatesData.results[0];
+            return { latitude, longitude, name };
+        } else {
+            throw new Error("No coordinates found for the specified city.");
+        }
+    
+    } catch (error) {
+        console.error("Error fetching coordinates:", error.message);
+        return null; // Return null or you could throw the error again depending on your needs
+    }
+}
 // Varna coordinates
 getWeather(43.21, 27.91).then(console.log);
+getCoordinates("Tokyo").then(console.log);
--- APIsPracticing/WeatherAppWithAPI/app.js
+++ APIsPracticing/WeatherAppWithAPI/app.js
@@ -1,7 +1,32 @@
-import { getWeather } from "./api.js";
+import { getWeather,getCoordinates } from "./api.js";
 import { renderWeather, renderError } from "./ui.js";
 
+const inputCity=document.getElementById("city-input");
+const searchBtn=document.getElementById("search-btn");
 
+async function handleSearch(){
+    const inputValue=inputCity.value.trim();
+    if(inputValue){
+        try{
+            const {latitude, longitude, name}=await getCoordinates(inputValue);
+            if(latitude && longitude){
+                const weatherData=await getWeather(latitude, longitude);
+                const objectToRender={
+                    cityName:name,
+                    temperature: weatherData.temperature,
+                    windspeed: weatherData.windspeed
+                }
+                renderWeather(objectToRender);
+            }
+        }
+        catch(error){
+            renderError("Failed to load weather data for the specified city.");
+        }
+    }
+    else{
+         renderError("Please enter a city name.");
+    }
+}
 async function init(){
     try {
         const weatherData=await getWeather(43.21, 27.91);
@@ -19,3 +44,4 @@ async function init(){
     }
 }
 init();
+searchBtn.addEventListener("click", handleSearch);
